e107995b3b7316c27ff83ab7218c1dd50af81656
liuchao <ry2u@outlook.com>
2018-03-10 17:45:37 +0800

    tortoise

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -6,6 +6,7 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Dispaly(Name:"Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
