4f4a342b4b91d598b3310055d6c8e657499a4a62
liuchao <ry2u@outlook.com>
2018-03-11 21:32:11 +0800

    Migration

--- MvcMovie/MvcMovie/Migrations/20180311124953_Rating.Designer.cs
+++ MvcMovie/MvcMovie/Migrations/20180311124953_Rating.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20180311124953_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
